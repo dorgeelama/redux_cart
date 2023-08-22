8a2eb04875b66abd49a37023197557b782ea83d1
dorgee <dorgeelama91@gmail.com>
2023-08-22 16:04:50 -0600

    cart container done

---
+++ starter/src/App.js
@@ -0,0 +1,11 @@
+import Navbar from './components/Navbar';
+import CartContainer from './components/CartContainer';
+function App() {
+  return (
+    <>
+      <Navbar />
+      <CartContainer />
+    </>
+  );
+}
+export default App;
---
+++ starter/src/components/CartItem.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const CartItems = (props) => {
+  return <div>CartItems</div>;
+};
+
+export default CartItems;
